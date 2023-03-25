af70a5fa5223a206047058070eecbc7fe98e93e9
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-03-25 18:04:05 -0300

    mais organizado

--- Exemplo01/Principal.cpp
+++ Exemplo01/Principal.cpp
@@ -2,28 +2,7 @@
 
 #include "stdafx.h"
 
-Principal::Principal()
-    : Simao(3, 10, 1976, "Jean Simão"),
-      Einstein(13, 3, 1879, "Albert Einstein"),
-      Newton(4, 1, 1643, "Isaac Newton") {
-    UTFPR.setNome("UNIVERSIDADE TECNOLOGICA");
-    Princeton.setNome("Princenton");
-    Cambridge.setNome("Cambridge");
-    DAINF.setNomeDepartamento("Dainf");
-    FisicaPrinceton.setNomeDepartamento("Fisica Princeton");
-    MatematicaCambridge.setNomeDepartamento("Matematica Cambridge");
-
-    UTFPR.setDepAssociado(&DAINF);
-    Princeton.setDepAssociado(&FisicaPrinceton);
-    Cambridge.setDepAssociado(&MatematicaCambridge);
-
-    Simao.setUnivFiliado(&UTFPR);
-    Simao.setDptFiliado(&DAINF);
-    Einstein.setUnivFiliado(&Princeton);
-    Einstein.setDptFiliado(&FisicaPrinceton);
-    Newton.setUnivFiliado(&Cambridge);
-    Newton.setDptFiliado(&MatematicaCambridge);
-
+Principal::Principal() {
     struct tm *local;
     time_t segundos;
     time(&segundos);
@@ -31,15 +10,121 @@ Principal::Principal()
     diaAtual = local->tm_mday;
     mesAtual = local->tm_mon + 1;
     anoAtual = local->tm_year + 1900;
-
+    Inicializa();
     Executar();
 }
 
-void Principal::Executar() {
+void Principal::Inicializa() {
+    InicializaUniversidades();
+    InicializaDepartamentos();
+    InicializaDisciplinas();
+    // InicializaAlunos();
+    InicializaProfessores();
+}
+
+void Principal::InicializaUniversidades() {
+    UTFPR.setNome("Universidade Tecnologica");
+    Princeton.setNome("Princenton");
+    Cambridge.setNome("Cambridge");
+    UFPR.setNome("Universidade Federal");
+
+    // "Filiando" as universidades a departamentos
+    UTFPR.setDepAssociado(&DAINF);
+    UTFPR.setDepAssociado(&DAELN);
+    Princeton.setDepAssociado(&FisicaPrinceton);
+    Cambridge.setDepAssociado(&MatematicaCambridge);
+    UFPR.setDepAssociado(&DEARTES);
+}
+
+void Principal::InicializaDepartamentos() {
+    // inicializa os objetos departamentos
+    DAINF.setNomeDepartamento("informatica");
+    FisicaPrinceton.setNomeDepartamento("fisica");
+    MatematicaCambridge.setNomeDepartamento("matematica");
+    DEARTES.setNomeDepartamento("artes");
+    DAELN.setNomeDepartamento("eletronica");
+
+    // "Filiando" os departamentos a universidades
+
+    DAELN.setUniversidade(&UTFPR);
+    DAINF.setUniversidade(&UTFPR);
+    DEARTES.setUniversidade(&UFPR);
+    FisicaPrinceton.setUniversidade(&Princeton);
+    MatematicaCambridge.setUniversidade(&Cambridge);
+
+    // "Filiando" os departamentos a disciplinas
+    DAINF.incluirDisciplina(&Calculo);
+    DAINF.incluirDisciplina(&Algoritmos);
+    FisicaPrinceton.incluirDisciplina(&Fisica);
+    MatematicaCambridge.incluirDisciplina(&Calculo);
+    DEARTES.incluirDisciplina(&Artes);
+    DAELN.incluirDisciplina(&Eletronica);
+}
+
+void Principal::InicializaProfessores() {
+    // inicializa os objetos professores
+    Simao.inicializa(3, 10, 1976, "Jean Simão");
+    Einstein.inicializa(13, 3, 1879, "Albert Einstein");
+    Newton.inicializa(4, 1, 1643, "Isaac Newton");
+
+    // "Filiando" os professores a universidades
+    Simao.setUnivFiliado(&UTFPR);
+    Einstein.setUnivFiliado(&Princeton);
+    Newton.setUnivFiliado(&Cambridge);
+
+    // "Filiando" os professores a departamentos
+    Simao.setDptFiliado(&DAINF);
+    Einstein.setDptFiliado(&FisicaPrinceton);
+    Newton.setDptFiliado(&MatematicaCambridge);
+}
+
+void Principal::InicializaDisciplinas() {
+    // inicializa os objetos disciplinas
+    Calculo.setNome("Calculo");
+    Fisica.setNome("Fisica");
+    Algoritmos.setNome("Algoritmos");
+    Artes.setNome("Artes");
+    Eletronica.setNome("Eletronica");
+
+    // "Filiando" as disciplinas a departamentos
+    Calculo.setDpto(&DAINF);
+    Fisica.setDpto(&FisicaPrinceton);
+    Algoritmos.setDpto(&DAINF);
+    Artes.setDpto(&DEARTES);
+    Eletronica.setDpto(&DAELN);
+}
+
+// Funcoes do executar
+
+void Principal::CalcIdadeProfs() {
     Simao.calcIdadeImprime(diaAtual, mesAtual, anoAtual);
     Einstein.calcIdadeImprime(diaAtual, mesAtual, anoAtual);
     Newton.calcIdadeImprime(diaAtual, mesAtual, anoAtual);
+}
+
+void Principal::UnivOndeProfsTrabalham() {
     Simao.ondeTrabalho();
     Einstein.ondeTrabalho();
     Newton.ondeTrabalho();
+}
+
+void Principal::DepOndeProfsTrabalham() {
+    Simao.qualDptTrabalho();
+    Einstein.qualDptTrabalho();
+    Newton.qualDptTrabalho();
+}
+
+void Principal::ListeDiscDptos() {
+    DAINF.listarDisciplinas();
+    FisicaPrinceton.listarDisciplinas();
+    MatematicaCambridge.listarDisciplinas();
+    DEARTES.listarDisciplinas();
+    DAELN.listarDisciplinas();
+}
+
+void Principal::Executar() {
+    CalcIdadeProfs();
+    UnivOndeProfsTrabalham();
+    DepOndeProfsTrabalham();
+    ListeDiscDptos();
 }
--- Exemplo01/Principal.h
+++ Exemplo01/Principal.h
@@ -1,18 +1,19 @@
-#include "Pessoa.h"
+#include "Aluno.h"
+#include "Departamento.h"
+#include "Disciplina.h"
+#include "Professor.h"
+#include "Universidade.h"
 
 class Principal {
    private:
-    Pessoa Simao;
-    Pessoa Newton;
-    Pessoa Einstein;
+    Professor Simao;
+    Professor Newton;
+    Professor Einstein;
 
-    Universidade UTFPR;
-    Universidade Princeton;
-    Universidade Cambridge;
+    Universidade UTFPR, Princeton, Cambridge, UFPR;
+    Departamento DAINF, FisicaPrinceton, MatematicaCambridge, DEARTES, DAELN;
 
-    Departamento DAINF;
-    Departamento FisicaPrinceton;
-    Departamento MatematicaCambridge;
+    Disciplina Calculo, Fisica, Algoritmos, Artes, Eletronica;
 
     int diaAtual;
     int mesAtual;
@@ -20,5 +21,18 @@ class Principal {
 
    public:
     Principal();
+
+    void Inicializa();
+    void InicializaUniversidades();
+    void InicializaDepartamentos();
+    void InicializaDisciplinas();
+    // void InicializaAlunos();
+    void InicializaProfessores();
+
     void Executar();
+
+    void CalcIdadeProfs();
+    void UnivOndeProfsTrabalham();
+    void DepOndeProfsTrabalham();
+    void ListeDiscDptos();
 };
